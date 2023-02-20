ed723a26528aeeecb1f936f00d589e7ea0ce1089
Alexander Matveev <sa-matik@yandex.com>
2023-02-20 12:02:54 +0300

    first commit

---
+++ conftest.py
@@ -0,0 +1,22 @@
+import time
+import pytest
+from selenium import webdriver
+from selenium.webdriver import ChromeOptions
+
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='en',
+                     help="Choose user interface language for websites. ")
+
+
+@pytest.fixture(scope ='function')
+def browser(request):
+    language = request.config.getoption("language")
+    print(f"Selected language: {language}")
+    options = ChromeOptions()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+    driver = webdriver.Chrome(options=options)
+    yield driver
+    time.sleep(3)
+    driver.quit()
