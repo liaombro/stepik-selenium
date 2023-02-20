7411d4049cc4683510f46201d8d294e07d5827e1
Alexander Matveev <sa-matik@yandex.com>
2023-02-20 16:11:11 +0300

    Add product page object and tests

--- page_objects/locators.py
+++ page_objects/locators.py
@@ -8,3 +8,11 @@ class LoginPageLocators:
     URL = 'http://selenium1py.pythonanywhere.com/ru/accounts/login/'
     LOGIN_FORM = (By.ID, "login_form")
     REGISTER_FORM = (By.ID, "register_form")
+    
+class ProductPageLocators:
+    URL = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
+    ADD_TO_CART_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    SUCCESS_MESSAGE = (By.XPATH, "//*[contains(@class, 'alert-success')][1]")
+    PRODUCT_TITLE = (By.CSS_SELECTOR, ".product_main h1")
+    CART_TOTAL_MESSAGE = (By.CSS_SELECTOR, ".alert-info strong")
+    PRODUCT_PRICE = (By.CSS_SELECTOR,  ".product_main .price_color")
